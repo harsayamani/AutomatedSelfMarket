0256875ec1e9487456c7c9289b60dc789850f65b
harsayamani <harsayamani@gmail.com>
2020-06-17 17:25:28 +0700

    migrate fix

--- database/migrations/2020_03_12_020000_create_keranjang_table.php
+++ database/migrations/2020_03_12_020000_create_keranjang_table.php
@@ -14,14 +14,15 @@ class CreateKeranjangTable extends Migration
     public function up()
     {
         Schema::create('keranjang', function (Blueprint $table) {
+            $table->engine = 'MyISAM';
             $table->increments('id_keranjang');
             $table->string('id_produk');
             $table->foreign('id_produk')->references('id_produk')->on('produk');
-            $table->unsignedInteger('kuantitas');
+            $table->integer('kuantitas');
             $table->bigInteger('harga_update');
             $table->string('id_pelanggan');
             $table->foreign('id_pelanggan')->references('id_pelanggan')->on('pelanggan');
-            $table->unsignedInteger('status', 1);
+            $table->integer('status', 1);
             $table->timestamps();
         });
     }
